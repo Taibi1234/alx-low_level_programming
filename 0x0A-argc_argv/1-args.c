50d7db654a70dfbeb14f0d39cc3c04e2e39ba690
Taibi1234 <nourddintaibi26@gmail.com>
2024-01-14 03:05:39 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *arg[])
+int main(int argc, char *argv[])
 {
-	(void)argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);
+(void)argv; /* Ignore argv */
+printf("%d\n", argc - 1);
 
-	return (0);
+return (0);
 }
