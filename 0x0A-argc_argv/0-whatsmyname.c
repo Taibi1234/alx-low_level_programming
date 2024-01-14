c3dea83651fcfc004d0d44f0b9cb18c243d1d000
Taibi1234 <nourddintaibi26@gmail.com>
2024-01-14 08:15:08 -0800

    Fix typos in 0-whatsmyname.c and main.h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - print the name of progam follow by a new line.
  * @argc: number of command line arguments
  * @argv: array that contains the command line arguments
- * Return: Alawys 0  (Success)
+ * Return: Alawys 0 (Success)
  */
 int main(int argc  __attribute__((unused)), char  *argv[])
 {
