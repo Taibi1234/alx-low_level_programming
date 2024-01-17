455797c705af7e881fc2acecbf0e8f0b192113c0
Taibi1234 <nourddintaibi26@gmail.com>
2024-01-17 08:05:23 -0800

    Update

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -2,17 +2,17 @@
 /**
  * _strchar - program rntry point
  * @s: input value
- * @c: input value
+ * @accept: input value
  * Return: Always 0 (Success)
  */
-char *_strchar(char *s, char c)
+char *_strchar(char *s, char *accept)
 {
 	int a = 0;
 
 	for (; s[a] >= '\0'; a++)
 	{
-		if (s[a] == c)
-			return (&s[c]);
+		if (s[a] == accept)
+			return (&s[accept]);
 	}
 	return (0);
 }
